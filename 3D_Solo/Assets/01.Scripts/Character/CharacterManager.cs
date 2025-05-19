85e763ae3b6dff58a6382dc4e0ca1f8786f121b1
hkY333 <kimabc0630@naver.com>
2025-05-19 20:55:59 +0900

    feat : CharacterManager

--- 3D_Solo/Assets/01.Scripts/Character/CharacterManager.cs
+++ 3D_Solo/Assets/01.Scripts/Character/CharacterManager.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static CharacterManager instance;
+    public static CharacterManager Instance
     {
-        
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameObject("CharacterManager").AddComponent<CharacterManager>();
+            }
+            return instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public Player Player
     {
-        
+        get { return _player; }
+        set { _player = value; }
+    }
+
+    private Player _player;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 }
