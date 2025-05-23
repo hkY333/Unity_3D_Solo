a18723dab4429866229e3eb3badd478faae98b7a
hkY333 <kimabc0630@naver.com>
2025-05-23 11:59:30 +0900

    feat : 아이템 적용을 위한 Player.cs 수정
    +인스펙터 창에 바인딩

--- 3D_Solo/Assets/01.Scripts/Character/Player.cs
+++ 3D_Solo/Assets/01.Scripts/Character/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public PlayerController controller;
+    public PlayerCondition condition;
+
+    public ItemData itemData;
+    public Action addItem;
+
 
     private void Awake()
     {
         CharacterManager.Instance.Player = this;
         controller = GetComponent<PlayerController>();
+        condition = GetComponent<PlayerCondition>();
     }
 
     // Start is called before the first frame update
